8c7907506eb37156fe7c0311855c97a470e7624b
Trevor <trevorperry143@gmail.com>
2022-10-25 10:43:14 -0500

    favorites and delete

---
+++ project-2/src/Components/FavoriteCard.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function FavoriteCard({favorite, removeFavorite}) {
+    const {category, correct_answer, difficulty, question, id} = favorite
+
+    function handleDelete() {
+        fetch(`http://localhost:3001/Favorites/${id}`, {
+            method: "Delete",
+        })
+        removeFavorite(id)
+    }
+
+
+
+    return (
+        <div>
+            <p>Question:</p>
+            <p>{category}</p>
+            <p>{difficulty}</p>
+            <p>{question}</p>
+            <p>{correct_answer}</p>
+            <button onClick={handleDelete}>Remove</button>
+        </div>
+    )
+}
+
+export default FavoriteCard
