1b8d4797661edab0c9915ff4a05e399b8254394a
Mariia <m.goncharenkoo@icloud.com>
2022-10-25 11:06:36 -0400

    deleted small bugs

--- project-2/src/Components/App.js
+++ project-2/src/Components/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import AddQuestionForm from "./AddQuestionForm"
 import GenerateQuestionForm from "./GenerateQuestionForm";
 import NavBar from "./NavBar";
-import FavoriteQuestions from "./FavoriteQuestions";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <NavBar />
       <GenerateQuestionForm />
       <AddQuestionForm />
-      <FavoriteQuestions />
     </div>
   );
 }
