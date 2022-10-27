7a30c0f2e03f8845fd5e49ccc86b2791aabb3cbd
Adriel Puello <adrielpuello@gmail.com>
2022-10-26 20:29:24 -0400

    finishing touches on css

--- project-2/src/Components/App.js
+++ project-2/src/Components/App.js
@@ -4,6 +4,8 @@ import GenerateQuestionForm from "./GenerateQuestionForm";
 import NavBar from "./NavBar";
 import FavoriteQuestions from "./FavoriteQuestions";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import logo from '/home/adrielpuello/Development/Code/phase-2/project-2-random-questions/project-2/src/logo.png'
+
 // import topBarImage from '/home/trevor14/Development/code/project-2/project-2-random-questions/project-2/src/robotic top bar picture.png'
 
 function App() {
@@ -23,6 +25,7 @@ function App() {
   return (
   <div className="App">
     <BrowserRouter>
+    <img className="logo" src={logo} alt="logo"/>
       <NavBar />
       <Routes>
         <Route exact path="/" element={<GenerateQuestionForm handleAddFavorite={handleAddFavorite}/>}/>
